60a35f8bfe5fe28d166d0a6cdf5a03d70418340f
PerfectProgrammingQueen <dhyanadesai10@gmail.com>
2024-03-03 16:27:41 -0800

    added os detection

--- .venv/SYonos/test.py
+++ .venv/SYonos/test.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import numpy as np
 import os
+from sys import platform
 
-os.chdir('C:\\Users\\anany\\Downloads')
+#print (platform)
+
+if platform == "darwin":
+    os.chdir('/Users/dhyanadesai/Downloads')
+else:
+    os.chdir('C:\\Users\\anany\\Downloads')
+
+
 file = 'FullData2.csv'
 fields = ['ID', 'TRUEAGE' ,'DTH00_02', 'B11_2', 'B21_2','B27_2','B26_2', 'B24_2', 'D11H_2',
           'DTH02_05', 'B11_5', 'B21_5','B27_5','B26_5', 'B24_5', 'D11H_5',
@@ -18,7 +26,7 @@ df.columns = ['ID', 'AGE_SURVEY', 'DEATH_2', 'OP_QUAL_2', 'OP_BRIGHT_2', 'OP_YOU
               'DEATH_8', 'OP_QUAL_8', 'OP_BRIGHT_8', 'OP_YOUNG_8', 'OP_USE_8', 'SO_LONE_8', 'SO_ACT_8',
               'DEATH_11', 'OP_QUAL_11', 'OP_BRIGHT_11', 'OP_YOUNG_11', 'OP_USE_11', 'SO_LONE_11', 'SO_ACT_11']
 
-
+#hi
 # print(df.head(5))
 
 
